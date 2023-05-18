7642ac452a5c4eadd22162a8ac9a8718706b8061
ngocon <thanhconngo18@gmail.com>
2023-05-18 21:51:54 +0700

    Lam bai tap so 1

---
+++ Bai1.sql
@@ -0,0 +1,14 @@
+create database Testing_system_Assignmet_1;
+use Testing_system_Assignmet_1;
+create table Department(
+    DepartmentID     varchar(10),
+    DepartmentName   varchar(50)
+);
+create table `Position`(
+    PositionID       varchar(50),
+    PositionName     varchar(50)
+);
+create table Account(
+    AccountID        varchar(50),
+    Email            varchar(50)
+    ); 
