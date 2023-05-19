6566360fff40c9168d08f993a9e01d4dd6a9147e
ngocon <thanhconngo18@gmail.com>
2023-05-19 10:45:15 +0700

    Bai so 1

--- Bai1.sql
+++ Bai1.sql
@@ -1,14 +1,71 @@
 create database Testing_system_Assignmet_1;
 use Testing_system_Assignmet_1;
 create table Department(
-    DepartmentID     varchar(10),
+    DepartmentID     int,
     DepartmentName   varchar(50)
 );
 create table `Position`(
-    PositionID       varchar(50),
+    PositionID       int,
     PositionName     varchar(50)
 );
 create table Account(
-    AccountID        varchar(50),
-    Email            varchar(50)
-    ); 
+    AccountID        int,
+    Email            int,
+    Username         varchar(50),
+    Fullname         varchar(50),
+    DepartmentID     int,
+    PositionID       int,
+    CreateDate       date
+); 
+create table `Group`(
+    GroupID          int,
+    GroupName        varchar(50),
+    CreatorID        int,
+    CreateDate       date
+);
+create table `GroupAccount`(
+    GroupID           int,
+    AccountID         int,
+    JoinData          date
+);
+create table TypeQuestion(
+    TypeID            int,
+    TypeName          varchar(50)
+);
+create table CategoryQuestion(
+    CategoryID        int,
+    CategoryName      varchar(50)
+);
+create table Question(
+    QuestionID         int,
+    Content            varchar(250),
+    CategoryID         int,
+    TypeID             int,
+    CreatorID          int,
+    CreateDate         date
+);
+create table Answer(
+    AnswerID           int,
+    Content            varchar(250),
+    QuestionID         int,
+    isCorrect          varchar(50)
+);
+create table Exam(
+     ExamID            int,
+     Code              int,
+     Title             varchar(50),
+     CategoryID        int,
+     Duration          date,
+     CreatprID         int,
+     CreateDate        date
+);
+create table ExamQuestioin(
+     ExamID            int,
+     QuestionID        int
+);
+
+     
+     
+     
+
+
